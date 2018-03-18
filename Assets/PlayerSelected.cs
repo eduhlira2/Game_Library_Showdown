f6bcdadf7d7f3c58be428c223ccaee3d8664314f
eduhlira2 <eduardu.silva@hotmail.com>
2018-03-17 22:48:24 -0400

    Ajuste dos Assets Antigos

---
+++ Assets/PlayerSelected.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSelected : MonoBehaviour {
+	public GameObject[] players;
+	public int[] ids;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (gameObject);
+	}
+	
+	public void SetPlayer(GameObject player, int id){
+		for (int i = 0; i <= 3; i++) {
+			if (id == i+1) {
+				players [i] = player;
+				ids [i] = id;
+			}
+		}
+	}
+
+	public void CleanPlayer(int id){
+		for (int i = 0; i <= 3; i++) {
+			if (ids [i] == id) {
+				players [i] = null;
+				ids [i] = 0;
+			}
+		}
+	}
+}
